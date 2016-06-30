7e8cbfa3d3574909d9811a279e6399d36a1a973c
Giacomo Delfini <giacomo.delfini@consoft.it>
2016-06-30 17:04:02 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+//in main.ts vengono presentate le dipendenze principali
+//@angular è una cartella di sistema
+
+//platform-browser-dynamic contiene funzionalità specifiche
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
@@ -7,4 +11,5 @@ if (environment.production) {
 }
 
 bootstrap(AppComponent);
-
+//l'avvio dell'applicazione Angular avviene semplicemente invocando
+//il metodo bootstrap sul root dell'applicazione. In questo caso App Component
